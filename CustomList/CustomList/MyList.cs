842e444c7ce31556f1e445243bacce62a77eab13
sehenry <sarahelhe@gmail.com>
2018-02-15 20:58:45 -0600

    the methods in my MyList class have been stubbed out, 3 of them so far. A test class has been added and referrenced to the MyList class. I've created 3 tests so far for the Add method, which will add an object to the list. Still working on the TestMethodAddDouble which will determine whether the added object to the list is in the correct index. Working on indexer. TestMethodRemove has some coding complete, but isn't finished yet. MyList not implements an IEnumerable so one or more methods in the class can iterate.

---
+++ CustomList/CustomList/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class MyList <T>: IEnumerable <T>
+    {
+        public int Count; 
+        public T Add(T value)
+        {
+            return value;
+        }
+        public T Remove(T value)
+        {
+            return value;
+        }
+
+        //public IEnumerator<T> IterateList() can I not call the IEnumerator something else?
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomList/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace MyListTest
+{
+    [TestClass]
+    public double[] numbersAdded = new double[6];
+
+    public class MyListTesting
+    {
+        [TestMethod]
+        public void TestMethodAddInt()
+        {
+            MyList<int> add = new MyList<int>() { 1, 2, 3, 4, 5};
+                     
+            int addedToList = 6;        
+                 
+            add.Add(addedToList);
+
+            Assert.AreEqual(6, add.Count);
+        }
+        [TestMethod]
+        public void TestMethodAddString()
+        {
+            MyList<string> add = new MyList<string> { "hello", "world", "sarah", "is", "awesome" };
+
+            string addedToList = "koda";
+
+            add.Add(addedToList);
+
+            Assert.AreEqual(6, add.Count);
+        }
+        [TestMethod]
+        public void TestMethodAddDouble()
+        {
+            MyList<double> add = new MyList<double> { 23.1, 31.9, 1.77, 6.799, 10.09 };
+
+            double addedToList = 1000.14;
+            double i = 0;
+            add.Add(addedToList);
+
+            Assert.AreEqual(add[5], addedToList); //check that addedToList is at index [5]
+        }
+
+        [TestMethod]
+        public void TestMethodRemove()
+        {
+            MyList<int> subtract = new MyList<int>();
+
+            int removeFromList = 3;
+
+            subtract.Remove(removeFromList);
+
+            Assert.AreEqual(-1, subtract.Count);
+        }
+        [TestMethod]
+        public void TestIterateList()
+        {
+
+        }
+    }
+}
